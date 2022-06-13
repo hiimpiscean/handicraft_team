28c1e882aabf6d4bd93a2c5f1a3d6b30a7faedb1
Piscean <conanchihieu@gmail.com>
2022-06-13 23:49:11 +0700

    Checkout from 20220613

--- resources/views/masters/handicraftMasterwithRepos.blade.php
+++ resources/views/masters/handicraftMasterwithRepos.blade.php
@@ -5,16 +5,33 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
-        crossorigin="anonymous">
-  <link rel="stylesheet" href="{{asset('css/bookstyle.css')}}">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+        integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+ <!-- <link rel="stylesheet" href="{{asset('css/bookstyle.css')}}"> -->
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+  <!--owl slider stylesheet -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+  <!-- nice select  -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/css/nice-select.min.css" integrity="sha512-CruCP+TD3yXzlvvijET8wV5WxxEh5H8P4cmz0RFbKK6FlZ2sYl3AEsKlLPHbniXKSrDdFewhbmBK5skbdsASbQ==" crossorigin="anonymous" />
+  <!-- font awesome style -->
+  <link href="css/font-awesome.min.css" rel="stylesheet" />
+
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css/responsive.css" rel="stylesheet" />
+  <link rel="shortcut icon" href="images/favicon.png" type="">
   <title>Handicraft</title>
 </head>
 
-<body>
-<header>
+<body class="sub_page">
+<div class="hero_area">
+  <div class="bg-box">
+    <img src="images/hero-bg.jpg" alt="">
+  </div>
+<header class="header_section">
   {{--  @if(1 + 1 == 2)--}}
   {{--    @include('partials.bookNav')--}}
   {{--  @else--}}
@@ -23,19 +40,41 @@
   @include('partials.handicraftNavWithRepos')
 
 </header>
-
+</div>
 <main role="main">
   @yield('main')
 </main>
 
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+
+{{--nếu trang nào có scrip riêng thì sẽ loat vào đây--}}
+<!-- jQery -->
+<script src="js/jquery-3.4.1.min.js"></script>
+<!-- popper js -->
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+</script>
+<!-- bootstrap js -->
+<script src="js/bootstrap.js"></script>
+<!-- owl slider -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
+</script>
+<!-- isotope js -->
+<script src="https://unpkg.com/isotope-layout@3.0.4/dist/isotope.pkgd.min.js"></script>
+<!-- nice select -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/js/jquery.nice-select.min.js"></script>
+<!-- custom js -->
+<script src="js/custom.js"></script>
+<!-- Google Map -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh39n5U-4IoWpsVGUHWdqB6puEkhRLdmI&callback=myMap">
+</script>
+<!-- End Google Map -->
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
         crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"
-        integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF"
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF"
         crossorigin="anonymous"></script>
-{{--nếu trang nào có scrip riêng thì sẽ loat vào đây--}}
+
 @yield('script')
 
 
--- resources/views/partials/handicraftNavWithRepos.blade.php
+++ resources/views/partials/handicraftNavWithRepos.blade.php
@@ -1,63 +1,64 @@
-<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-  <div class="container">
 
-    <a class="navbar-brand"
-      {{--       href="{{route('book.index')}}"--}}
-    >HandicraftWeb</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
-            aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="bookDropdown" role="button" data-toggle="dropdown"
-             aria-haspopup="true" aria-expanded="false">Handicraft </a>
-          <div class="dropdown-menu" aria-labelledby="bookDropdown">
-            <a class="dropdown-item"
-               href="{{route('handicraft.index')}}"
-            >View All</a>
-            <a class="dropdown-item"
-               href="{{route('handicraft.create')}}"
-            >New Handicraft</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item"
-              {{--               href="{{route('book.reset')}}"--}}
-            >Reset data</a>
-          </div>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="publisherDropdown" role="button" data-toggle="dropdown"
-             aria-haspopup="true" aria-expanded="false">
-            Admin
-          </a>
-          <div class="dropdown-menu" aria-labelledby="publisherDropdown">
-            <a class="dropdown-item" href="{{route('admin.index')}}">View All</a>
-            <a class="dropdown-item" href="{{route('admin.create')}}">New Admin</a>
-          </div>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="formatDropdown" role="button" data-toggle="dropdown"
-             aria-haspopup="true" aria-expanded="false">
-            Category
-          </a>
-          <div class="dropdown-menu" aria-labelledby="formatDropdown">
-            <a class="dropdown-item" href="{{route('category.index')}}">View All</a>
-            <a class="dropdown-item" href="{{route('category.create')}}">New category</a>
-          </div>
-        </li>
+      <div class="container">
+        <nav class="navbar navbar-expand-lg custom_nav-container ">
+          <a class="navbar-brand"
+            {{--       href="{{route('book.index')}}"--}}
+          ><span>Handicraft</span></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="bookDropdown" role="button" data-toggle="dropdown"
+                   aria-haspopup="true" aria-expanded="false">Handicraft </a>
+                <div class="dropdown-menu" aria-labelledby="bookDropdown">
+                  <a class="dropdown-item"
+                     href="{{route('handicraft.index')}}"
+                  >View All</a>
+                  <a class="dropdown-item"
+                     href="{{route('handicraft.create')}}"
+                  >New Handicraft</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item"
+                    {{--               href="{{route('book.reset')}}"--}}
+                  >Reset data</a>
+                </div>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="publisherDropdown" role="button" data-toggle="dropdown"
+                   aria-haspopup="true" aria-expanded="false">
+                  Admin
+                </a>
+                <div class="dropdown-menu" aria-labelledby="publisherDropdown">
+                  <a class="dropdown-item" href="{{route('admin.index')}}">View All</a>
+                  <a class="dropdown-item" href="{{route('admin.create')}}">New Admin</a>
+                </div>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="formatDropdown" role="button" data-toggle="dropdown"
+                   aria-haspopup="true" aria-expanded="false">
+                  Category
+                </a>
+                <div class="dropdown-menu" aria-labelledby="formatDropdown">
+                  <a class="dropdown-item" href="{{route('category.index')}}">View All</a>
+                  <a class="dropdown-item" href="{{route('category.create')}}">New category</a>
+                </div>
+              </li>
 
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="formatDropdown" role="button" data-toggle="dropdown"
-             aria-haspopup="true" aria-expanded="false">
-            Customer
-          </a>
-          <div class="dropdown-menu" aria-labelledby="formatDropdown">
-            <a class="dropdown-item" href="{{route('customer.index')}}">View All</a>
-            <a class="dropdown-item" href="{{route('customer.create')}}">New Customer</a>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="formatDropdown" role="button" data-toggle="dropdown"
+                   aria-haspopup="true" aria-expanded="false">
+                  Customer
+                </a>
+                <div class="dropdown-menu" aria-labelledby="formatDropdown">
+                  <a class="dropdown-item" href="{{route('customer.index')}}">View All</a>
+                  <a class="dropdown-item" href="{{route('customer.create')}}">New Customer</a>
+                </div>
+              </li>
+            </ul>
           </div>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+        </nav>
+      </div>
+
